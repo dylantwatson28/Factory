52a044c9c43a922bc073c651b86b6ea455f2a5c9
Dylan Watson <dylanwatson@mac.milton.edu>
2025-11-11 14:59:11 -0500

    Initial commit, made factory class, instance variables, and constructor

---
+++ src/Factory.java
@@ -0,0 +1,46 @@
+public class Factory {
+    //instance variables
+    public int yearFounded;
+    public boolean isOpen;
+    //double (decimal number)
+    public double earnings;
+    //string
+    public String products;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Dylan's Factory");
+        //have to have new
+        //psvm
+        new Factory();
+
+
+
+    }
+        //constructor
+        public Factory(){
+            System.out.println(" we make houses!"); // we make houses = string
+            yearFounded = 2025;
+            isOpen = false;
+            earnings = 1.1;
+            products = "houses";
+            factoryInfo();
+
+
+
+    }
+    public void factoryInfo(){
+        System.out.println("the factory info: ");
+        System.out.println(products);
+        System.out.println("the year founded: ");
+        System.out.println(yearFounded);
+        System.out.println("the earnings: " + earnings);
+        System.out.println("it is " + isOpen + " that the factory is open");
+
+    }
+
+}
+//constructor diff type of method because it doesnt have a void and matches name of class
+//instance variables are made at top of class and have public and they can be used everywhere inside factory instead of psvm
+//todo: make method called factory info
+//todo: add souts to print the factory's instance variables
+//todo: change the yearFounded (value) and isOpen and call factoryInfo()
